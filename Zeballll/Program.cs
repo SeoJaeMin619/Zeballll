8369eed08c18f49b33901c16395fbdd64f0c16e5
SeoJaeMin619 <emiya1663@naver.com>
2023-08-18 15:38:46 +0900

    Character Issue

--- Zeballll/Program.cs
+++ Zeballll/Program.cs
@@ -1,55 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Reflection;
+﻿using System.Reflection;
 using System;
 
 class Program
 {
     private static Character player;
 
-    static void Main(string[] args)
-    {
-        Setting();
-        Intro();
-    }
+        static void Main(string[] args)
+        {
+            Setting();
+            Intro();
+        }
 
-    static void Setting()
-    {
-        // 캐릭터 정보 세팅
-        player = new Character("Jamnini", "돚거", 279, 2763, 37, 420, 1000);
+        static void Setting()
+        {
+            // 캐릭터 정보 세팅
+            player = new Character("Jamnini", "돚거", 279, 2763, 37, 420, 1000);
 
-        // 아이템 정보 세팅
-    }
+            // 아이템 정보 세팅
+        }
 
-    static void Intro()
-    {
-        Console.WriteLine();
+        static void Intro()
+        {
+            Console.WriteLine();
 
-        Console.WriteLine("커닝시티에 오신것을 환영합니다");
-        Console.WriteLine("파티퀘스트 '첫번째동행' 에 들어가기 전 활동을 할 수 있습니다");
-        Console.WriteLine();
-        Console.WriteLine("1. 상태보기");
-        Console.WriteLine("2. 인벤토리");
-        Console.WriteLine();
-        Console.WriteLine("원하시는 행동을 입력해주세요.");
-    }
+            Console.WriteLine("커닝시티에 오신것을 환영합니다");
+            Console.WriteLine("파티퀘스트 '첫번째동행' 에 들어가기 전 활동을 할 수 있습니다");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+        }
 
     public void Character()
-    {
+    //왜 캐릭터 인식을 못하지 . . .???
+    { 
         public string Name { get; }
-    public string Job { get; }
-    public int Level { get; }
-    public int Atk { get; }
-    public int Def { get; }
-    public int Hp { get; }
-    public int Gold { get; }
+        public string Job { get; }
+        public int Level { get; }
+        public int Atk { get; }
+        public int Def { get; }
+        public int Hp { get; }
+        public int Gold { get; }
+    }
 }
 
     //인트로 클래스
-    //캐릭정보창을 갈수있게 하기
+    //캐릭정보창을 갈수있게 하자
 
     //캐릭정보창 클래스
 
     //강화(?) 클래스
 
 
-}
+
+
