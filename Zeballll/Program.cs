e11fecc03447c313bdef1207135e98fb4803bb18
SeoJaeMin619 <emiya1663@naver.com>
2023-08-21 17:08:49 +0900

    샘플 코드를 보며 수정

--- Zeballll/Program.cs
+++ Zeballll/Program.cs
@@ -1,60 +1,113 @@
-﻿using System.Reflection;
-using System;
-
-class Game
+﻿internal class Program
 {
     private static Character player;
 
-        static void Main(string[] args)
+    static void Main(string[] args)
+    {
+        Setting();
+        Intro();
+    }
+
+    static void Setting()
+    {
+        // 캐릭터 정보 세팅
+        player = new Character("Jamnini", "도적", 279, 2763, 37, 420, 1000);
+
+        // 아이템 정보 세팅
+    }
+
+    static void Intro()
+    {
+        Console.Clear();
+
+        Console.WriteLine("커닝시티에 오신 것을 환영합니다");
+        Console.WriteLine("파티퀘스트 '첫번째 동행' 에 들어가기 전 활동을 하실 수 있습니다.");
+        Console.WriteLine();
+        Console.WriteLine("1. 상태보기");
+        Console.WriteLine("2. 인벤토리");
+        Console.WriteLine();
+        Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+        int input = CheckValidInput(1, 2);
+        switch (input)
         {
-            Setting();
-            Intro();
+            case 1:
+                Info();
+                break;
+
+            case 2:
+                // 작업해보기
+                break;
         }
- 
-        static void Setting()
+    }
+
+    static void Info()
+    {
+        Console.Clear();
+
+        Console.WriteLine("상태보기");
+        Console.WriteLine("캐릭터의 능력치를 표시합니다.");
+        Console.WriteLine();
+        Console.WriteLine($"Lv.{player.Level}");
+        Console.WriteLine($"{player.Name}({player.Job})");
+        Console.WriteLine($"공격력 :{player.Atk}");
+        Console.WriteLine($"방어력 : {player.Def}");
+        Console.WriteLine($"체력 : {player.Hp}");
+        Console.WriteLine($"Gold : {player.Gold} G");
+        Console.WriteLine();
+        Console.WriteLine("0. 나가기");
+
+        int input = CheckValidInput(0, 0);
+        switch (input)
         {
-            // 캐릭터 정보 세팅
-            player = new Character("Jamnini", "돚거", 279, 2763, 37, 420, 1000);
-
-            // 아이템 정보 세팅
-
+            case 0:
+                Intro();
+                break;
         }
+    }
 
-        static void Intro()
+    static void DisplayInventory()
+    {
+
+    }
+
+    static int CheckValidInput(int min, int max)
+    {
+        while (true)
         {
-            Console.WriteLine();
+            string input = Console.ReadLine();
 
-            Console.WriteLine("커닝시티에 오신것을 환영합니다");
-            Console.WriteLine("파티퀘스트 '첫번째동행' 에 들어가기 전 활동을 할 수 있습니다");
-            Console.WriteLine();
-            Console.WriteLine("1. 상태보기");
-            Console.WriteLine("2. 인벤토리");
-            Console.WriteLine();
-            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            bool parseSuccess = int.TryParse(input, out var ret);
+            if (parseSuccess)
+            {
+                if (ret >= min && ret <= max)
+                    return ret;
+            }
 
+            Console.WriteLine("잘못된 입력입니다.");
         }
-        static void Info()
-        //캐릭터 정보창
-        {
-            Console.WriteLine();
-            Console.WriteLine("체력")
-        }
-
-    class Character()
-    //인식하지만 이름이랑 직업등등 인식을 못하는 중
-    { 
-        public string Name { get; }
-        public string Job { get; }
-        public int Level { get; } 
-        public int Atk { get; }
-        public int Def { get; }
-        public int Hp { get; }
-        public int Gold { get; }
     }
 }
 
-    
 
+public class Character
+{
+    public string Name { get; }
+    public string Job { get; }
+    public int Level { get; }
+    public int Atk { get; }
+    public int Def { get; }
+    public int Hp { get; }
+    public int Gold { get; }
 
-
-
+    public Character(string name, string job, int level, int atk, int def, int hp, int gold)
+    {
+        Name = name;
+        Job = job;
+        Level = level;
+        Atk = atk;
+        Def = def;
+        Hp = hp;
+        Gold = gold;
+    }
+}
